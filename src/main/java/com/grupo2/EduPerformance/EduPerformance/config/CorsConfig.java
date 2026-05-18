0a494ca320e258e81dc2db30778a914c745ed774
Yeisson Angel <yeisonossa3010@gmail.com>
2026-05-17 20:28:48 -0500

    Cambios en COrsCOnfig para la solicitud de cualquier lado

--- src/main/java/com/grupo2/EduPerformance/EduPerformance/config/CorsConfig.java
+++ src/main/java/com/grupo2/EduPerformance/EduPerformance/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:4200", "http://localhost:8081", "http://localhost:5173") // Puertos comunes de React (CRA/Vite), Angular y Vue
+                .allowedOriginPatterns("*") // Permite cualquier origen (IDE online, AI Studio, local)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
